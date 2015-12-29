2bd2d41b52a430a314d3270795147ef88cbffdde
Fattyman <01723950755@vodafone.de>
2015-12-29 01:22:49 +0100

    Refactoring

--- js/clockclock/cyphers/view.js
+++ js/clockclock/cyphers/view.js
@@ -9,35 +9,40 @@ function CypherView(){
 		nodeHand : null,
 		color : null,
 		drawAllCypherParts : function(animationSequences){
-			this.animationSequences=animationSequences;
+			this.animationSequences = animationSequences;
 			this.drawCypherBackground();
 			this.drawCypherForeground();
 		},
 		drawCypherBackground : function(){
-			this.cbx.drawImage(this.frontBuffer,0,0);
+			this.cbx.drawImage(this.backBuffer, 0, 0);
 		},
 		drawCypherForeground : function(){
-			this.cbx.strokeStyle=this.color;
+			this.cbx.strokeStyle = this.color;
 			this.drawCompleteCypher();
-			this.ctx.drawImage(this.backBuffer,0,0);
+			this.ctx.drawImage(this.frontBuffer, 0, 0);
 		},
 		drawCompleteCypher : function(){
-			var cyphers=0;
-			doForAllCypherParts(this,function(lineNo,colNo){
-				this.drawTwoClockHands(colNo,lineNo,cyphers++);
+			var cyphers = 0;
+			doForAllCypherParts(this, function(lineNo, colNo){
+				this.drawTwoClockHands(colNo, lineNo, cyphers++);
 				cyphers++;
 			})
 		},
 		drawTwoClockHands : function(x,y,cyphers){
-			this.drawClockHand(x,y,cyphers);
-			this.drawClockHand(x,y,cyphers+1);
+			this.drawClockHand(x, y, cyphers);
+			this.drawClockHand(x, y, cyphers+1);
 		},
-		drawClockHand : function(x,y,cyphers){
+		drawClockHand : function(x, y, cyphers){
 			ClockClockDrawUtils.drawAngleLine(
-				this.cbx, this.radius,
-				x, y,
-				ClockClockUtils.getStartOfAnimation(cyphers,this.animationSequences),
-				this.hand-6, this.color, this.nodeHand, true );
+				this.cbx, 
+				this.radius,
+				x, 
+				y,
+				ClockClockUtils.getStartOfAnimation(cyphers, this.animationSequences),
+				this.hand-6, 
+				this.color, 
+				this.nodeHand, 
+				true );
 		}
 	};
 }
